136ac64ea88b3700a7f8e727c8aa71837f13598a
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-01-29 21:42:26 +0100

    Suppression du composant Hero2 de la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import Feature from "@/src/blocks/Features";
 import Footer from "@/src/blocks/Footer";
 import Hero from "@/src/blocks/Hero";
-import Hero2 from "@/src/blocks/Hero2";
 import Navbar from "@/src/blocks/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
-      <Hero2 />
       <Hero />
       <Feature />
       <Footer />
