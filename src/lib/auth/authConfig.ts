36ed020bcba2552d4111f5c86e099111b9301faa
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-01-29 21:44:59 +0100

    Ajout de la configuration d'authentification avec NextAuth et intégration des fournisseurs GitHub et Google

---
+++ src/lib/auth/authConfig.ts
@@ -0,0 +1,63 @@
+// auth.ts
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "../prisma";
+import bcrypt from "bcrypt";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email et mot de passe requis !");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (!user) {
+          throw new Error("Utilisateur non trouvé !");
+        }
+
+        // Vérifier le mot de passe avec bcrypt
+        const isValidPassword = await bcrypt.compare(credentials.password as string, user.password as string);
+        if (!isValidPassword) {
+          throw new Error("Mot de passe incorrect !");
+        }
+
+        return user;
+      },
+    }),
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub!;
+      }
+      return session;
+    },
+  },
+});
+
+
+
