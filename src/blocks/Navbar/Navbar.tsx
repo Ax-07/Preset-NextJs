0a870bdb1384f81891e3539f873c2c8425f4dcd0
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-01-30 22:09:17 +0100

    Suppression du type explicite pour le composant Navbar afin de simplifier le code

--- src/blocks/Navbar/Navbar.tsx
+++ src/blocks/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { DesktopNavbar } from "./DesktopNavbar";
 import { MobileNavbar } from "./MobileNavbar";
 import { useMediaQuery } from "@/src/hooks/useMediaQuery";
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
   const isDesktop = useMediaQuery("(min-width: 1024px)");
   const fixedNav = "fixed top-0 left-0 z-10 w-full bg-background";
 
