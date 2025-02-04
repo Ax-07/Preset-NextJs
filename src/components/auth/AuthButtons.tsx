67369a4798fc283a032e0fb1a86b568308bdf30e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2025-02-04 21:16:16 +0100

    Refactor les boutons d'authentification pour utiliser des hooks personnalisés et simplifier la logique de connexion

--- src/components/auth/AuthButtons.tsx
+++ src/components/auth/AuthButtons.tsx
@@ -1,29 +1,19 @@
-"use client"
+// @/src/components/auth/AuthButtons.tsx
+"use client";
 
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import GithubIcon from "@/src/assets/github-mark/github-mark-white.svg";
 import { Button } from "@/src/components/ui/button";
 import Image from "next/image";
-import { Avatar, AvatarFallback } from "@/src/components/ui/avatar";
 import Link from "next/link";
-import { AvatarImage } from "@radix-ui/react-avatar";
-import { signIn, signOut, useSession } from "next-auth/react";
-import userIconSvg from "@/src/assets/faUser.svg";
-import { cn } from "@/src/utils/tailwind_cn";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuTrigger,
-} from "@/src/components/ui/dropdown-menu";
-import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
+import { LogOut } from "lucide-react";
+import { useAuth } from "@/src/hooks/useAuth";
+import { DeleteUserAction } from "@/src/lib/auth/auth.actions";
 
 export const LoginWithGithubButton = () => {
-  const router = useRouter();
+  const { loginWithGithub } = useAuth();
 
   return (
     <Button
@@ -31,10 +21,7 @@ export const LoginWithGithubButton = () => {
       variant="outline"
       type="submit"
       className="mt-2 w-full"
-      onClick={async () => {
-        await signIn("github");
-        router.push("/");
-      }}
+      onClick={loginWithGithub}
     >
       <Image
         src={GithubIcon}
@@ -49,7 +36,7 @@ export const LoginWithGithubButton = () => {
 };
 
 export const LoginWithGoogleButton = () => {
-  const router = useRouter();
+  const { loginWithGoogle } = useAuth();
 
   return (
     <Button
@@ -57,10 +44,7 @@ export const LoginWithGoogleButton = () => {
       variant="outline"
       type="submit"
       className="mt-2 w-full"
-      onClick={async () => {
-        await signIn("google");
-        router.push("/");
-      }}
+      onClick={loginWithGoogle}
     >
       <FcGoogle className="mr-2 size-5" />
       Login with Google
@@ -69,24 +53,23 @@ export const LoginWithGoogleButton = () => {
 };
 
 export const LogoutButton = () => {
-  const router = useRouter();
   return (
     <Button
       color="primary"
       variant="default"
       type="submit"
       className="w-full"
-      onClick={async() => {
-       await signOut();
-        router.push("/");
+      onClick={async () => {
+        await signOut({ redirectTo: "/" });
       }}
     >
+      <LogOut className="mr-2 size-5" />
       Logout
     </Button>
   );
 };
 
-export const SignInButtons = () => {
+export const LoginButtons = () => {
   return (
     <Link href="/auth">
       <Button variant="outline" size="sm">
@@ -96,49 +79,17 @@ export const SignInButtons = () => {
   );
 };
 
-type AuthButtonsProps = {
-  className?: string;
+type DeleteAccountButtonProps = {
+  userId: string;
 };
-
-export const AuthButtons: React.FC<AuthButtonsProps> = ({ className }) => {
-  const { data: session } = useSession(); console.log(session);
-
-  if (!session?.user) {
-    return <SignInButtons />;
+export const DeleteAccountButton = ({userId}: DeleteAccountButtonProps) => {
+  const onDeleteAccount = async (userId: string) => {
+    await DeleteUserAction(userId);
+    await signOut();
   }
-
   return (
-    <div className={cn("flex items-center gap-4", className)}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild className="cursor-pointer">
-          <Avatar className="size-9 items-center justify-center ">
-            {session.user.image ? (
-              <>
-                <AvatarFallback>{session.user.name?.[0]}</AvatarFallback>
-                <AvatarImage
-                  src={session.user.image}
-                  alt={`${session.user.name ?? "-"}'s profile picture`}
-                />
-              </>
-            ) : (
-              <Image src={userIconSvg} alt="User icon" className="size-6" />
-            )}
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-48">
-          <DropdownMenuLabel>Bonjour! {session.user.name}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <LogoutButton />
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
-  );
-};
+    <Button variant="destructive" size="sm" onClick={() => onDeleteAccount(userId!)}>
+      Delete account
+    </Button>
+  )
+}
